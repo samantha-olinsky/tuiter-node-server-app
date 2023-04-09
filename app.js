8a7e85e2e719a2c05caec78d308b9a2b61fcc600
Samantha Olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2023-04-09 18:30:49 -0400

    finished a9

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ import HelloController from './controllers/hello-controller.js';
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express()
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 HelloController(app)
 UserController(app)
 TuitsController(app)
+
 app.listen(process.env.PORT || 4000);
+
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+  topic: String,
+  userName: String,
+  handle: String,
+  time: String,
+  image: String,
+  title: String,
+  tuit: String,
+  likes: Number,
+  liked: Boolean,
+  dislikes: Number,
+  disliked: Boolean,
+  replies: Number,
+  retuits: Number,
+
+}, {collection: 'tuits'});
+export default schema;
